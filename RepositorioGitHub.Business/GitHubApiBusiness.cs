da71b0f5276f5941735ba7bf8c8205d87978f99c
matteus-henryk <matteus.henryk2@gmail.com>
2022-04-05 20:01:38 -0300

    Adicionar arquivos de projeto.

---
+++ RepositorioGitHub.Business/GitHubApiBusiness.cs
@@ -0,0 +1,49 @@
+﻿using RepositorioGitHub.Business.Contract;
+using RepositorioGitHub.Dominio;
+using RepositorioGitHub.Dominio.Interfaces;
+using RepositorioGitHub.Infra.Contract;
+using System;
+
+namespace RepositorioGitHub.Business
+{
+    public class GitHubApiBusiness: IGitHubApiBusiness
+    {
+        private readonly IContextRepository _context;
+        private readonly IGitHubApi _gitHubApi;
+        public GitHubApiBusiness(IContextRepository context, IGitHubApi gitHubApi)
+        {
+            _context = context;
+            _gitHubApi = gitHubApi;
+        }
+
+        public ActionResult<GitHubRepositoryViewModel> Get()
+        {
+            return new ActionResult<GitHubRepositoryViewModel>();
+        }
+
+        public ActionResult<GitHubRepositoryViewModel> GetById(long id)
+        {
+            return new ActionResult<GitHubRepositoryViewModel>();
+        }
+
+        public ActionResult<RepositoryViewModel> GetByName(string name)
+        {
+            return new ActionResult<RepositoryViewModel>();
+        }
+
+        public ActionResult<FavoriteViewModel> GetFavoriteRepository()
+        {
+            return new ActionResult<FavoriteViewModel>();
+        }
+
+        public ActionResult<GitHubRepositoryViewModel> GetRepository(string owner, long id)
+        {
+            return new ActionResult<GitHubRepositoryViewModel>();
+        }
+
+        public ActionResult<FavoriteViewModel> SaveFavoriteRepository(FavoriteViewModel view)
+        {
+            return new ActionResult<FavoriteViewModel>();
+        }
+    }
+}
---
+++ RepositorioGitHub.Infra/ApiGitHub/GitHubApi.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using RepositorioGitHub.Dominio;
+using RepositorioGitHub.Dominio.Interfaces;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositorioGitHub.Infra.ApiGitHub
+{
+    public class GitHubApi : IGitHubApi
+    {
+        public ActionResult<GitHubRepository> GetRepository(string owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult<RepositoryModel> GetRepositoryByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
